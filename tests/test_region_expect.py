697caa2ba421b5529f35d3f17c3a8c3f4ae565ab
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2025-12-10 15:41:09 +0200

    Perform identical DPI and scaling for Qt6 apps for all OS-es
    Use environment variables for the fixed DPI settings that should
    make pixel counts independent of the environment screen.
    
    Fix zero margins and spacing for the Qt6 image window but adapt
    the locations once while taking the other possible approach for
    the Qt6 app - keep the prevous locations but fix the margins and
    spacing to match the previous ones for certain default displays.
    Zero spacing and magins was too ugly for the Qt6 application but
    not that relevant for the simpler Qt6 image. Also use fixed and
    more universal fonts in a more standard way (not stylesheet).
    
    Additionally sort the Qt imports better and only load the Qt app
    if running as a script.

--- tests/test_region_expect.py
+++ tests/test_region_expect.py
@@ -121,15 +121,15 @@ class RegionTest(unittest.TestCase):
         self.show_image('all_shapes')
 
         match = self.region.find(Image('shape_green_box'))
-        self.assertAlmostEqual(match.x, 30, delta=5)
-        self.assertAlmostEqual(match.y, 190, delta=5)
+        self.assertAlmostEqual(match.x, 20, delta=5)
+        self.assertAlmostEqual(match.y, 180, delta=5)
         self.assertAlmostEqual(70, match.width, delta=5)
         self.assertAlmostEqual(50, match.height, delta=5)
 
         # Match again - this time just pass a filename
         match = self.region.find('shape_green_box')
-        self.assertAlmostEqual(match.x, 30, delta=5)
-        self.assertAlmostEqual(match.y, 190, delta=5)
+        self.assertAlmostEqual(match.x, 20, delta=5)
+        self.assertAlmostEqual(match.y, 180, delta=5)
         self.assertAlmostEqual(70, match.width, delta=5)
         self.assertAlmostEqual(50, match.height, delta=5)
 
@@ -182,14 +182,14 @@ class RegionTest(unittest.TestCase):
         greenbox = Image('shape_green_box')
         matches = self.region.find_all(greenbox)
         self.assertEqual(len(matches), 1)
-        self.assertAlmostEqual(matches[0].x, 30, delta=5)
-        self.assertAlmostEqual(matches[0].y, 190, delta=5)
+        self.assertAlmostEqual(matches[0].x, 20, delta=5)
+        self.assertAlmostEqual(matches[0].y, 180, delta=5)
         self.assertAlmostEqual(70, matches[0].width, delta=5)
         self.assertAlmostEqual(50, matches[0].height, delta=5)
 
         redbox = Image('shape_red_box')
         matches = self.region.find_all(redbox)
-        expected_matches = [(25, 25), (320, 25), (315, 115)]
+        expected_matches = [(15, 15), (310, 15), (305, 105)]
         self.assertEqual(len(matches), len(expected_matches))
         for match in matches:
             self.region.hover(match)
@@ -205,7 +205,7 @@ class RegionTest(unittest.TestCase):
         self.region.cv_backend.matcher.params["template"]["nocolor"].value = False
         matches = self.region.find_all(pinkbox)
         # approximately the above coordinates since maching different needle
-        expected_matches = [(25, 35), (320, 40), (320, 125), (30, 255)]
+        expected_matches = [(15, 25), (310, 30), (305, 120), (20, 245)]
         self.assertEqual(len(matches), len(expected_matches))
         for match in matches:
             self.region.hover(match)
@@ -219,7 +219,7 @@ class RegionTest(unittest.TestCase):
         self.region.cv_backend.matcher.params["find"]["similarity"].value = 0.8
         self.region.cv_backend.matcher.params["template"]["nocolor"].value = True
         matches = self.region.find_all(pinkbox)
-        expected_matches = [(30, 120), (30, 195), (30, 255)]
+        expected_matches = [(20, 110), (20, 185), (20, 245)]
         self.assertEqual(len(matches), len(expected_matches))
         for match in matches:
             self.region.hover(match)
